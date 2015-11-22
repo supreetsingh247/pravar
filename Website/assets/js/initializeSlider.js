c791db328da1521526aa3966fdb85f57e1801c5f
Supreet Singh <supreetsingh.247@gmail.com>
2015-11-22 16:41:35 +0530

    Added new slider

---
+++ Website/assets/js/initializeSlider.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Savy on 22-Nov-15.
+ */
+$(document).ready(function(){
+    $('.autoplay').slick({
+        accessibility:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+});
